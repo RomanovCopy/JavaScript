1201cd44240576f500bdc6b23d42d0a1cc9798e3
Сергей Романов <RomanovCopy@gmail.com>
2024-08-09 14:24:23 +0300

    fix0907241425

--- 03_JS_base/script.js
+++ 03_JS_base/script.js
@@ -1,3 +1,30 @@
 $(function(){
-    console.log($('#container').css('color','red'));
+    
+    $('#change_color').on('click', function(){
+        $('#container').css('color','red');
+    });
+    $('#container').on('mouseover', function(){
+        $('#container').css('color','red');
+    });
+    $('#container').on('mouseout', function(){
+        $('#container').css('color','green');
+    });
+    $('#container').on('dblclick', function(){
+        $('#container').css('color','blue');
+    });
+
+
+    $('textarea').on('keydown', function(event){
+        if(event.which < 48 || event.which > 57){
+            return false;
+        }
+    });
+
+    $('textarea').on('focus', function(event){
+        $('textarea').css('background-color', 'green')
+    });
+
+    $('textarea').on('blur', function(event){
+        $(this).css('background-color', 'red')
+    });
 });

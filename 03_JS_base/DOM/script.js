034e297c0f4ebb7fe519d3a897578fca2dad712b
RomanovCopy <RomanovCopy@gmail.com>
2024-08-11 22:02:09 +0300

    fix1108242202

--- 03_JS_base/DOM/script.js
+++ 03_JS_base/DOM/script.js
@@ -10,7 +10,33 @@ $(function(){
             }
         });
     });
+    // $('.news-time').click(function(){
+    //     $(this).parent().parent().css('color', 'blue');
+    // });
+    // $('.news-time').click(function(){
+    //     $(this).parents('.news-item').css('color', 'blue');
+    // });
     $('.news-time').click(function(){
-        $(this).parent().parent().css('color', 'blue');
+        var header=$('#column-left h2');
+        if(header.css('display')=='block'){
+            // header.css('display', 'none');
+            header.hide();
+        }else{
+            // header.css('display', 'block');
+            header.show();
+        }
     });
+
+     $('#popup-button').click(function(){
+        $('#popup-container').fadeIn(400);
+    });
+    $('#popup-container').click(function(event){
+         if(event.target==this){
+            $(this).fadeOut(400);
+         }
+        
+    });
+
 });
+
+0:14:12

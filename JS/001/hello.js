aa4f4af1c110838c03ff64cac6d9bef7cb6fdf09
RomanovCopy <RomanovCopy@gmail.com>
2024-08-14 21:57:10 +0300

    fix1408242157

--- JS/001/hello.js
+++ JS/001/hello.js
@@ -4,10 +4,15 @@ var myBirthYear = prompt('Введите ваш год рождения');
 var currentYear = 2024;
 var age = currentYear - myBirthYear;
 
-if (age < 20) {
+if (age > 0 && age < 20) {
     alert('Привет, ' + myName + '!');
 } else if (age > 20 && age < 40) {
     alert('Добрый день, ' + myName + '!');
-} else {
+} else if( age > 40 ) {
     alert('Здравствуйте, ' + myName + '!');
+}else{
+    alert('Вы из будущего? Это не возможно!!!')
 }
+
+
+

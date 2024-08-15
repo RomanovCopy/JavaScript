3891d5989cbd83514cf6055f39bebd3c7c05ec15
RomanovCopy <RomanovCopy@gmail.com>
2024-08-15 20:55:24 +0300

    fix1508242055

--- JS/004_ArraysAndFunctions/script.js
+++ JS/004_ArraysAndFunctions/script.js
@@ -1,4 +1,18 @@
 'use strict';
 
+//var anotherCar=new Object();
 
- 
+ var car = {
+    mark: 'BMV',
+    model:'X5',
+    'year of manufacture': 2010,
+
+    getName: function(){
+        return this.mark + ' ' + this.model;
+    }
+ };
+
+ console.log(car.mark);
+ console.log(car.model);
+ console.log(car['year of manufacture']);
+ console.log(car.getName());

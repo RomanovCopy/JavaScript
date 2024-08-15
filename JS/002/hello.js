11d2d9d46ccf1d1a8d5a4f56ada917f5fda88651
Сергей Романов <RomanovCopy@gmail.com>
2024-08-15 10:37:41 +0300

    002End

---
+++ JS/002/hello.js
@@ -0,0 +1,17 @@
+var count=0;
+while(count<5){
+    console.log(count);
+    count++;
+}
+console.log('End!!!');
+
+do{
+    console.log(count);
+    count--;
+}while(count>0);
+
+
+for(var i=0;i<5;i++){
+    console.log(i);
+}
+console.log(i);

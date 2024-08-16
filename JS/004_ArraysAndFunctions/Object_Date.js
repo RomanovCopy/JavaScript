525b40127b912647515a2e57f8c484c9964d9b97
RomanovCopy <RomanovCopy@gmail.com>
2024-08-16 21:08:08 +0300

    004_End

---
+++ JS/004_ArraysAndFunctions/Object_Date.js
@@ -0,0 +1,15 @@
+var d=new Date();
+
+//день недели(0-воскресенье, 6-суббота)
+d.getDay();
+
+d.getDate();//дата(число месяца)
+d.getMonth();//месяц( 0 - январь, 11- декабрь)
+d.getFullYear();//текущий год
+
+d.getHours()//часы
+d.getMinutes()//минуты
+d.getSeconds()//секунды
+
+
+ 

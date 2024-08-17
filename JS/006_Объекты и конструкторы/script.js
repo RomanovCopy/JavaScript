c8fc1de8cc68d9c1ffbeff4d579fd3efe9ae4da0
RomanovCopy <RomanovCopy@gmail.com>
2024-08-17 17:50:28 +0300

    fix1708241750

--- JS/006_Объекты и конструкторы/script.js
+++ JS/006_Объекты и конструкторы/script.js
@@ -1,3 +1,28 @@
+//функция-конструктор
+
+function User(firstName, lastName){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.isAdmin=false;
+
+    //локальная переменная, недоступна за пределами объекта
+    var localVar='';
+    //локальная функция, недоступна за пределами объекта
+    function test(){
+        
+    }
+
+    this.getFullName=function(){
+        return  this.firstName+' '+this.lastName;
+    }
+}
+
+//создание экземпляров объектов
+var user_01=new User('Sergey', 'Romanov');
+var user_02=new User('Veronika','Romanova');
+
+console.log(user_01.getFullName());
+console.log(user_02.getFullName());
 
 
  

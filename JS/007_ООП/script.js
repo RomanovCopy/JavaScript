5100f22194010115152000f7d5e297faeafaf171
RomanovCopy <RomanovCopy@gmail.com>
2024-08-18 19:30:47 +0300

    fix1808241930

---
+++ JS/007_ООП/script.js
@@ -0,0 +1,31 @@
+ 'use strict'
+ function User(firstName, lastName){
+
+    this.firstName=firstName;
+    this.lastName=lastName;
+    
+    this.getFullName=function(){
+        return this.firstName + ' ' + this.lastName;
+    }
+ }
+
+
+ function Manager(firstName, lastName){
+    console.log(arguments);//Arguments(2) ['Kristina', 'Romanova']
+    User.apply(this, arguments);//наследование от 
+    //User выполняя функцию-конструктор в текущем контексте
+
+    //собственная функция
+    this.sayHello=function(){
+        alert('Чем могу помочь?');
+    }
+
+    this.changeName=function(name){
+        this.firstName=name;
+    }
+ }
+
+ var user_01=new User('Sergey', 'Romanov');
+ var manager_01=new Manager('Kristina', 'Romanova');
+
+

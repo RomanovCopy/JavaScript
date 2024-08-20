eb147bc1333fd10790028e2f4b596c9b977b483b
Сергей Романов <RomanovCopy@gmail.com>
2024-08-20 10:32:23 +0300

    TimeOut_End

--- JS/008_Разное/script.js
+++ JS/008_Разное/script.js
@@ -1,19 +1,26 @@
-function showARGS(){
-    //преобразование arguments в Array
-    var args=Array.prototype.slice.call(arguments);
-    //или
-    var args1=[].slice.call(arguments);
-    //или
-    var args2=Array.from(arguments);
-    //или
-    var args3=[...arguments];
-    //slice - создает копию элементов argumments и помещает  их
-    //в новый (args) массив
+// function showARGS(){
+//     //преобразование arguments в Array
+//     var args=Array.prototype.slice.call(arguments);
+//     //или
+//     var args1=[].slice.call(arguments);
+//     //или
+//     var args2=Array.from(arguments);
+//     //или
+//     var args3=[...arguments];
+//     //slice - создает копию элементов argumments и помещает  их
+//     //в новый (args) массив
+//     console.log(arguments[0], arguments[1]);
+//     console.log(args.join(', '));
+//     console.log(args1.join(', '));
+//     console.log(args2.join(', '));
+//     console.log(args3.join(', '));
+// }
+
+// showARGS('Hellow', 'World');
+
+function display(){
+    console.log('Запуск функции с задержкой.');
     console.log(arguments[0], arguments[1]);
-    console.log(args.join(', '));
-    console.log(args1.join(', '));
-    console.log(args2.join(', '));
-    console.log(args3.join(', '));
 }
 
-showARGS('Hellow', 'World');
+setTimeout(display,1000, 'Hello', 'world');

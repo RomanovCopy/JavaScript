f169d283357781a70a7c6f5d2580157f1eafaf77
RomanovCopy <RomanovCopy@gmail.com>
2024-08-24 15:58:16 +0300

    008_End

--- JS/008_Разное/script.js
+++ JS/008_Разное/script.js
@@ -24,3 +24,49 @@
 // }
 
 // setTimeout(display,1000, 'Hello', 'world');
+
+
+ 
+// var str=prompt('ISON   ');
+// try{
+//     var json=JSON.parse(str);
+// }catch(ex){
+//     alert('Введен некорректный JSON');
+// }
+// console.log(json);
+
+// function calcByrthYear(){
+//     var age=prompt('Введите ваш возраст ');
+//     var birthYear=new Date().getFullYear()-age;
+//     if(isNaN(birthYear)){
+//         throw('Введено нечисловое значение');
+//     }else if (age!==null){
+//         alert('Ваш год рождения '+birthYear)
+//     }  
+// }
+
+// try{
+//     calcByrthYear();
+// }catch(ex){
+//     alert(ex);
+// }
+
+
+
+// if(/1/.test('2')){
+//     console.log(true);
+// }
+
+//  var phone=prompt('Номер телефона в формате: xxx xx xx');
+//  var isValidPhone=/\d{3} \d{2} \d{2}/
+//  alert(isValidPhone.test(phone));
+
+
+// var str='123afsrwt1524';
+// console.log(str.replace(/\d/g , ''));//удаление десятичный чисел
+// //g - применить ко всей строке
+
+
+
+
+
